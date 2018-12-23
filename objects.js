cb48f8794cde437973c21421103f334e9316f53c
Sabir Baloch <baloch_jan01@live.com>
2018-12-23 23:14:18 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,5 +1,7 @@
 var playlist={
 Adele:'Hello!'
 }
-var updatePlaylist=(playlist,artistname,songtitle)=>{
+var updatePlaylist=(playlist,artistname,Hello)=>{
+playlist['Adele']='Hello'
+return playlist;
 }
